b88489af07db54bbf3e28e5ae12662b037f1f90d
AXIOM <124213381+axiom-nsut@users.noreply.github.com>
2023-02-09 19:49:39 +0530

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Home from "./Home.jsx";
+import About from "./About.jsx"
+import Departments from "./Departments"
+import Explore from "./event.jsx";
+
+export {
+    Explore,
+    About,
+    Navbar,
+    Footer,
+    Testimonials,
+    Home,
+    Departments,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,63 @@
+import { instagram, people01, people02, people03, planet, planetb, planetc, planetd, planete } from "../assets";
+
+export const exploreWorlds = [
+    {
+        id: 'world-1',
+        imgUrl: {planet},
+        title: 'Know your friend',
+    },
+    {
+        id: 'world-2',
+        imgUrl: {planetb},
+        title: 'Ditch The Snitch',
+    },
+    {
+        id: 'world-3',
+        imgUrl: {planetc},
+        title: '2 Truth 1 lie',
+    },
+    {
+        id: 'world-4',
+        imgUrl: {planetd},
+        title: 'Weapons Master',
+    },
+    {
+        id: 'world-5',
+        imgUrl: {planete},
+        title: 'Survive The Apocalypse',
+    },
+];
+export const feedback = [
+    {
+        id: "feedback-2",
+        content:
+            "The only thing we have to fear is fear itself.",
+        name: "Divyanshu",
+        title: "Vice President",
+        img: people02,
+    }, {
+        id: "feedback-1",
+        content:
+            "Life is 10% what happens to us and 90% how we react to it.",
+        name: "Vehu",
+        title: "President",
+        img: people01,
+    },
+    {
+        id: "feedback-3",
+        content:
+            "The journey of a thousand miles begins with one step.",
+        name: "Gautam Tayal",
+        title: "Vice President",
+        img: people03,
+    },
+];
+
+
+export const socialMedia = [
+    {
+        id: "social-media-1",
+        icon: instagram,
+        link: "https://www.instagram.com/axiomnsut/",
+    },
+];
